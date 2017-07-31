f6218111e1f38974b0c60a36fd254230c5eee1cb
j2systems <pmichel@j2interactive.com>
2017-07-31 17:03:01 +0100

    Base

--- cgi-bin/bin/j2docker-init.sh
+++ cgi-bin/bin/j2docker-init.sh
@@ -12,7 +12,7 @@ do
 	SERVICECHK=$(systemctl is-active docker)
 done
 unset SERVICECHK
-
+[[ ! -f /var/www/cgi-bin/tmp ]] && mkdir /var/www/cgi-bin/tmp && chmod 777 /var/www/cgi-bin/tmp
 . /var/www/cgi-bin/bin/config.sh
 . /var/www/cgi-bin/bin/networking.sh
 . /var/www/cgi-bin/tmp/globals
