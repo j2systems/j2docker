ade2f48d677173cb859c421abc03ef7e2c5b8339
root <root@j2docker.lan>
2017-08-01 10:42:12 +0100

    Base

--- cgi-bin/bin/dockerhub.sh
+++ cgi-bin/bin/dockerhub.sh
@@ -5,6 +5,6 @@
 
 #Put master list in tmp
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${J2USER}'", "password": "'${J2PASS}'"}' https://hub.docker.com/v2/users/login/|jq -r .token)
-curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/j2systems/docker/tags/|jq -r '.results|.[]|"\(.name) \(.full_size) \(.last_updated)"'>tmp/dockerhub
-chmod 666 tmp/dockerhub
+curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/j2systems/docker/tags/|jq -r '.results|.[]|"\(.name) \(.full_size) \(.last_updated)"'>/var/www/cgi-bin/tmp/dockerhub
+chmod 666 /var/www/cgi-bin/tmp/dockerhub
 
