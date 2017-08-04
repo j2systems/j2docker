99bec54856737e1aa737425d8ec242f72dd05af9
root <root@j2docker.lan>
2017-08-04 13:34:50 +0100

    Added recursion to listing j2systems/docker images

--- cgi-bin/bin/dockerhub.sh
+++ cgi-bin/bin/dockerhub.sh
@@ -2,9 +2,19 @@
 #
 # Creates list of images on docker hub
 . /var/www/cgi-bin/tmp/globals
-
+[[ -f /var/www/cgi-bin/tmp/dockerhub ]] && rm -f /var/www/cgi-bin/tmp/dockerhub && touch /var/www/cgi-bin/tmp/dockerhub && chmod 666 /var/www/cgi-bin/tmp/dockerhub 
 #Put master list in tmp
+
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${J2USER}'", "password": "'${J2PASS}'"}' https://hub.docker.com/v2/users/login/|jq -r .token)
-curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/j2systems/docker/tags/|jq -r '.results|.[]|"\(.name) \(.full_size) \(.last_updated)"'>/var/www/cgi-bin/tmp/dockerhub
-chmod 666 /var/www/cgi-bin/tmp/dockerhub
+RECURSE=true
+THISURL=https://hub.docker.com/v2/repositories/j2systems/docker/tags/
+
+while [[ "${THISURL}" != "null" ]]
+do
+	NEWINFO=$(curl -s -H "Authorization: JWT ${TOKEN}" ${THISURL})
+	echo ${NEWINFO}|jq -r '.results|.[]|"\(.name) \(.full_size) \(.last_updated)"'>>/var/www/cgi-bin/tmp/dockerhub
+	THISURL=$(echo ${NEWINFO}|jq -r '.next')
+done
+
+
 
